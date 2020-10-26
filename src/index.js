73fbeef4b0267bac9fb39031ad1330fb5cd40acd
Albert Cedeno <albertcdn@gmail.com>
2020-10-26 09:29:54 -0400

    added search bar component and styling

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { render } from "react-dom";
 import { ApolloProvider } from "@apollo/client";
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import Wines from "./components/Wines";
+import Search from  "./components/Search";
 import "./index.css";
 
 const client = new ApolloClient({
@@ -14,6 +15,7 @@ const client = new ApolloClient({
 
 const App = () => (
   <ApolloProvider client={client}>
+    <Search />
     <Wines />
   </ApolloProvider>
 );
